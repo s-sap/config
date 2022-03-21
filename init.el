b1e6405d13234f9f7d6e53c8893a323b325ee59f
ssap <67758731+s-sap@users.noreply.github.com>
2022-03-20 21:24:35 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,48 +1,10 @@
-;; [ Emacs Config ]
-
-;;(require 'package)
-;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-;; emacs's packages
-;;(ac-config-default)
-;;(doom-modeline-mode 1)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Load Editor Theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/atom-one-dark-theme")
-(load-theme 'atom-one-dark t)
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+(package-install 'use-package))
 
-;; Custom Color
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "gray55" :slant oblique))))
- '(region ((t (:extend t :background "gray45")))))
-
-;; My Editor View
-(global-display-line-numbers-mode)
-(setq display-line-numbers-type 'relative)
-(global-hl-line-mode)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(setq c-default-style "linux")
-(setq c-basic-offset 4)
-(c-set-offset 'comment-intro 0)
-
-;; Emacs File Backup Save
-(setq backup-directory-alist `(("." . "~/.saves")))
-
-;; Indentation
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
-
-;; My Emacs key binding
-(global-set-key (kbd "C-t") 'tab-new)
-(global-set-key (kbd "C-x C-q") 'tab-close)
-(global-set-key (kbd "C-x C-a") 'toggle-fullscreen)
-(global-set-key (kbd "M-s") 'ibuffer)
-
+(org-babel-load-file (expand-file-name "~/.emacs.d/myInit.org"))
